e4a92c6aa7f4422fa93cc774d401f75afabd6dd7
agierens <andrew.gierens@bigpond.com>
2018-04-10 21:31:55 +1000

    Init

---
+++ stories/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Button from '../components/button';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button />
+  )) 
