4acb4d024e270b478ab3a4be666b3a863cb091b4
Andrew Gierens <apgierens@gmail.com>
2018-06-08 00:08:10 +1000

    Export (#11)
    * - Added: Exporting
    
    * v1.0.1
    
    * - Added: Index
    
    * v1.0.2
    
    * - Fixed: exports
    
    * - Inc'd version

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as Button } from "./dist/8bit/button";
+export { default as Toolbar } from "./dist/8bit/toolbar";
+export { default as Panel } from "./dist/8bit/panel";
+export { default as VerticalAligner } from "./dist/8bit/vertical-aligner";
--- src/8bit/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from "./button";
-export { default as Toolbar } from "./toolbar";
-export { default as Panel } from "./panel";
-export { default as VerticalAligner } from "./vertical-aligner";
