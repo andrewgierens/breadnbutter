541b7f4669d77faa5e9d26655e8033d754edd659
agierens <andrew.gierens@bigpond.com>
2018-06-07 23:24:44 +1000

    - Fixed: exports

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import * as breadnbutter from './dist';
-
-module.exports = breadnbutter;
+export { default as Button } from "./dist/8bit/button";
+export { default as Toolbar } from "./dist/8bit/toolbar";
+export { default as Panel } from "./dist/8bit/panel";
+export { default as VerticalAligner } from "./dist/8bit/vertical-aligner";
