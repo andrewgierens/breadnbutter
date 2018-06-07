eb9e7cba11c230b0cc630b026475e9b82f5d0766
agierens <andrew.gierens@bigpond.com>
2018-06-07 21:22:08 +1000

    - Enhanced: Removed glamour

--- src/8bit/common.ts
+++ src/8bit/common.ts
@@ -1,5 +1,5 @@
 import * as Color from "color";
-import { CSSProperties } from "glamorous";
+import { css } from 'emotion';
 
 // Some consties
 const shadowWidth = 0.2;
@@ -15,66 +15,68 @@ export const getForegroundColor = (rootColor: Color): string => {
   return rootColor.isDark() ? "white" : "black";
 };
 
-export const getClickable = (rootColor: Color): CSSProperties => {
+export const getClickable = (rootColor: Color): string => {
   const shadowColor = rootColor.darken(0.25).hex().toString();
 
-  const hoverAndFocus = {
-    boxShadow: `inset -${shadowWidth * 1.5}rem -${shadowWidth * 1.5}rem 0px 0px ${shadowColor}`,
-    outline: 0,
-  };
+  const hoverAndFocus = css`
+    box-shadow: inset -${ shadowWidth * 1.5 }rem -${shadowWidth * 1.5}rem 0px 0px ${shadowColor};
+    outline: 0;
+  `;
 
-  return {
-    "cursor": "pointer",
-    "boxShadow": `inset -${shadowWidth}rem -${shadowWidth}rem 0px 0px ${shadowColor}`,
-    ":hover": {
-      ...hoverAndFocus,
-      background: rootColor.darken(0.075).hex().toString(),
-    },
-    ":focus": hoverAndFocus,
-    ":active": {
-      boxShadow: `inset ${shadowWidth}rem ${shadowWidth}rem 0px 0px ${shadowColor}`,
-    },
-  } as CSSProperties;
+  return css`
+    cursor: pointer;
+    box-shadow: inset -${shadowWidth}rem -${shadowWidth}rem 0px 0px ${shadowColor};
+    &:hover: {
+      ${hoverAndFocus};
+      background: ${rootColor.darken(0.075).hex().toString()};
+    };
+    &:focus: {
+      ${hoverAndFocus};
+    };
+    &:active: {
+      box-shadow: inset ${shadowWidth}rem ${shadowWidth}rem 0px 0px ${shadowColor};
+    };
+  `;
 };
 
-export const get2dOutline = (rootColor: string = "white"): CSSProperties => {
-  const beforeAfter = {
-    content: " ",
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    boxSizing: "content-box",
-  };
+export const get2dOutline = (rootColor: string = "white"): string => {
+  const beforeAfter = css`
+    content: " ";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    box-sizing: content-box;
+  `;
 
   const mainColor = Color(rootColor);
   const borderColor = mainColor.isLight ? "black" : "white";
 
-  const before = {
-    top: `-${outlineWidth}rem`,
-    left: "0",
-    borderTop: `${outlineWidth}rem ${borderColor} solid`,
-    borderBottom: `${outlineWidth}rem ${borderColor} solid`,
-    pointerEvents: "none",
-  };
+  const before = css`
+    top: -${outlineWidth}rem;
+    left: 0;
+    border-top: ${outlineWidth}rem ${borderColor} solid;
+    border-bottom: ${outlineWidth}rem ${borderColor} solid;
+    pointer-events: none;
+  `;
 
-  const after = {
-    left: `-${outlineWidth}rem`,
-    top: "0",
-    borderLeft: `${outlineWidth}rem ${borderColor} solid`,
-    borderRight: `${outlineWidth}rem ${borderColor} solid`,
-    pointerEvents: "none",
-  };
+  const after = css`
+    left: -${outlineWidth}rem;
+    top: 0;
+    border-left: ${outlineWidth}rem ${borderColor} solid;
+    border-right: ${outlineWidth}rem ${borderColor} solid;
+    pointer-events: none;
+  `;
 
-  const style = {
-    ":after": {
-      ...beforeAfter,
-      ...after,
-    },
-    ":before": {
-      ...beforeAfter,
-      ...before,
-    },
-  };
+  const style = css`
+    &:after: {
+      ${beforeAfter};
+      ${after};
+    };
+    &:before: {
+      ${beforeAfter};
+      ${before};
+    };
+  `;
 
-  return style as CSSProperties;
+  return style;
 };
